56198b7d0159b322773dc729fde74d356461bde8
janhvibadwaik <badwaikjanhvi@gmail.com>
2026-05-09 11:33:03 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import './App.css';
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const itemPrice = 100;
+  const totalPrice = count * itemPrice;
 
   return (
     <div className="app">
       <div className="counter-card">
         <h1>Counter App</h1>
+        <div className="info-box">
+        <p>Items Added: {count}</p>
+        <p>Price Per Item: ₹{itemPrice}</p>
+        <p className="total-price">
+        Total Price: ₹{totalPrice}
+  </p>
+</div>
         <div className="count-display">{count}</div>
         <div className="button-group">
           <button className="btn btn-decrement"
-            onClick={() => setCount(count - 1)}
+            onClick={() => setCount(count > 0 ? count - 1 : 0)}
           >
             Decrement
           </button>
